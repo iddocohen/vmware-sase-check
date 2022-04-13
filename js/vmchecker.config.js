decbf0f5b7a982cfbea31346d7abd2606856b950
Iddo Cohen <iddocohen@gmail.com>
2022-04-13 19:05:03 +0200

    Fixing spelling mistakes

--- js/vmchecker.config.js
+++ js/vmchecker.config.js
@@ -89,10 +89,10 @@ const defaultTestConfig = [
         title: "Block access to websites with spyware and adware content",
         desc: "This test attempts to download a image from such a spyware website.",
         detail: "Spyware is a type of malware that steals sensitive information from unknowing users. The effects of spyware range from gathering usage habits for marketing purposes to targeted attacks by nation-states against political activists.",
-        how  : "In order to protect your organisation from spyware and adware, you can use <strong>Threat Categories</strong> under <strong>URL Filtering</strong> to select <strong>Malware</strong>.",
+        how  : "In order to protect your organization from spyware and adware, you can use <strong>Threat Categories</strong> under <strong>URL Filtering</strong> to select <strong>Malware</strong>.",
         fail : "The lack of proper security enforcement forcement can permit spyware to continually track user activities and sends this data to third-parties. With proper <strong>URL Filtering</strong> with VMware CWS, this can be avoided.",
         load : "Testing if the browser can reach a popular spyware website...",
-        id: "block_spwyware",
+        id: "block_spyware",
         category: "urlfilter",
         version: 1,
         property: "system",
@@ -109,7 +109,7 @@ const defaultTestConfig = [
         desc: "This test tries to download the well known EICAR file over HTTPS to check if it gets blocked.",
         detail: "The damages that an organization may experience as a victim of malware can be listed as, data breaches, illegal removal of balance from bank accounts and unauthorized access to valuable files/documents.",
         how  : "If not already done so, isEnabled the <strong>Inspection Engine</strong> first by going under <strong>Policies</strong>. Then create a policy that inspects <strong>Upload</strong> and <strong>Download</strong> for <strong>All files</strong> and <strong>All User Group</strong> and <strong>All Domain/Categories</strong>. Last but not least use <strong>Action</strong> and select <strong>Inspect</strong> to ensure <strong>File Hash Check</strong>, <strong>File Full Scan</strong> and <strong>Sandbox Inspection</strong> are used for fitlering. For more info please visit <a href='https://docs.vmware.com/en/VMware-Cloud-Web-Security/4.4/VMware-Cloud-Web-Security-Configuration-Guide/GUID-A48C9642-A96C-4CC5-90E9-7C5490378661.html' target='_blank' rel='noopener' class='link-light'>here</a>.",
-        fail : "To protect your organisations from 0-day or known, malware, spyware and viruse files, VMware CWS <strong>Content Inspection</strong> can be used to identify and block theese automatically.",
+        fail : "To protect your organisations from 0-day or known, malware, spyware and virus files, VMware CWS <strong>Content Inspection</strong> can be used to identify and block these automatically.",
         load : "Trying to download the eicar.com file over HTTPs...",
         id: "block_https_malware",
         category: "cinspect",
@@ -128,7 +128,7 @@ const defaultTestConfig = [
         desc: "This test tries to download the well known EICAR file over HTTP to check if it gets blocked.",
         detail:"Malware is a very powerful software that is a threat to the corporate organizations. The easy targets of malware are the multinational organizations. Primarily, organizations will face huge complications and internal damages if they fall prey to malware virus.",
         how  : "If not already done so, isEnabled the <strong>Inspection Engine</strong> first by going under <strong>Policies</strong>. Then create a policy that inspects <strong>Upload</strong> and <strong>Download</strong> for <strong>All files</strong> and <strong>All User Group</strong> and <strong>All Domain/Categories</strong>. Last but not least use <strong>Action</strong> and select <strong>Inspect</strong> to ensure <strong>File Hash Check</strong>, <strong>File Full Scan</strong> and <strong>Sandbox Inspection</strong> are used for fitlering. For more info please visit <a href='https://docs.vmware.com/en/VMware-Cloud-Web-Security/4.4/VMware-Cloud-Web-Security-Configuration-Guide/GUID-A48C9642-A96C-4CC5-90E9-7C5490378661.html' target='_blank' rel='noopener' class='link-light'>here</a>.",
-        fail : "To protect your organisations from 0-day or known, malware, spyware and viruse files, VMware CWS <strong>Content Inspection</strong> can be used to identify and block this automatically.",
+        fail : "To protect your organizations from 0-day or known, malware, spyware and virus files, VMware CWS <strong>Content Inspection</strong> can be used to identify and block this automatically.",
         load : "Trying to download the eicar.com file over HTTP...",
         id: "block_http_malware",
         category: "cinspect",
@@ -151,7 +151,7 @@ const defaultTestConfig = [
         desc: "This test attempts to download a malware from popular cloud application provider.",
         detail: "Netskope's new report in 2021 found that cloud storage apps account for more than 66% of cloud malware delivery. Cybercriminals deliver malware through cloud apps to bypass and take advantage of any app-specific allow lists.",
         how  : "If not already done so, isEnabled the <strong>Inspection Engine</strong> first by going under <strong>Policies</strong>. Then create a policy that inspects <strong>Upload</strong> and <strong>Download</strong> for <strong>All files</strong> and <strong>All User Group</strong> and <strong>All Domain/Categories</strong>. Last but not least use <strong>Action</strong> and select <strong>Inspect</strong> to ensure <strong>File Hash Check</strong>, <strong>File Full Scan</strong> and <strong>Sandbox Inspection</strong> are used for fitlering. For more info please visit <a href='https://docs.vmware.com/en/VMware-Cloud-Web-Security/4.4/VMware-Cloud-Web-Security-Configuration-Guide/GUID-A48C9642-A96C-4CC5-90E9-7C5490378661.html' target='_blank' rel='noopener' class='link-light'>here</a>.",
-        fail : "Organisations can become a potential big target and may face huge losses if, they do not take any effective to block malware from any kind of destination. VMware CWS can help with <strong>Content Inspection</strong> to reduce that risk.",
+        fail : "Organizations can become a potential big target and may face huge losses if, they do not take any effective to block malware from any kind of destination. VMware CWS can help with <strong>Content Inspection</strong> to reduce that risk.",
         load : "Trying to download the malware from AWS...",
         id: "block_cloud_malware",
         category: "cinspect",
@@ -242,7 +242,7 @@ const defaultTestConfig = [
      {
         title: "Not allow searching Video's (example Vimeo)",
         desc: "This test checks if searching videos on Vimeo get blocked and if other functionalities still work.",
-        detail: "Vimeo is one of the biggest plaform to share professional video for every device but as YouTube has a huge searchable video base which can motiviate procrastination. The VMware CWS Cloud Access Security Broker (CASB) can be used to allow employees to share companies video footage to Vimeo and in parallel deny browsing the content of Vimeo..",
+        detail: "Vimeo is one of the biggest platform to share professional video for every device but as YouTube has a huge search able video base which can motivate procrastination. The VMware CWS Cloud Access Security Broker (CASB) can be used to allow employees to share companies video footage to Vimeo and in parallel deny browsing the content of Vimeo..",
         how: "Under <strong>CASB</strong> in VMware CWS, use <strong>Add Rule</strong>, select <strong>All Users and Groups</strong> for source, select <strong>Vimeo</strong> under <strong>Custom Selection</strong>, choose <strong>Allow</strong> under <strong>Browser Action</strong> and choose <strong>Search</strong> to be <strong>Blocked</strong> only.",
         fail: "Users will be able to search and look for video's on Vimeo. With the right VMware CWS <strong>CASB</strong> configuration, this can be mitigated.",
         load : "Trying to execute a search in Vimeo...",
@@ -272,7 +272,7 @@ const defaultTestConfig = [
 const faqConfig = [
     {
         title: "How can I contribute?",
-        detail: "Feel free to fork the code and enhance it, test the extension and provide feedback or just give me a star in GitHub - all mechansims help to improve the extension or my motivation &#128521;",
+        detail: "Feel free to fork the code and enhance it, test the extension and provide feedback or just give me a star in GitHub - all mechanisms help to improve the extension or my motivation &#128521;",
         isEnabled: true
     },
     {
@@ -302,7 +302,7 @@ const faqConfig = [
             After confirming the above, it will do a <strong>performance</strong> test:<br><br>
                 <ul>
                     <li> It will use the domains defined in the file <code>js/vmchecker.config.js</code> within the variable name <code>testing_domains</code>.</li>
-                    <li> It will calculate the response time of each and calculate the total average, std and percentitle statistics.</li>
+                    <li> It will calculate the response time of each and calculate the total average, std and percentile statistics.</li>
                 </ul>
             <br>
             One can alter the <code>test_domains</code> variable to include or exclude domains for testing. Please only note, adding more domains, will cause more load to your browser.
